64ac53352835f9400971ba45e79fe8fe04e41066
yoosangyeob <syyoo@joara.com>
2022-08-22 19:00:18 +0900

    coroutine awaitAll test

--- src/samples/ConcurrencySample.kt
+++ src/samples/ConcurrencySample.kt
@@ -2,17 +2,30 @@ package samples
 
 import kotlinx.coroutines.*
 
-fun main() = runBlocking {
-    val deferred: Deferred<Int> = async(Dispatchers.Default) {
-        loadData()
-    }
-    log("waiting...")
-    log(deferred.await())
-}
+//fun main() = runBlocking {
+//    val deferred: Deferred<Int> = async(Dispatchers.Default) {
+//        loadData()
+//    }
+//    log("waiting...")
+//    log(deferred.await())
+//}
+//
+//suspend fun loadData(): Int {
+//    log("loading...")
+//    delay(1000L)
+//    log("loaded!")
+//    return 42
+//}
 
-suspend fun loadData(): Int {
-    log("loading...")
-    delay(1000L)
-    log("loaded!")
-    return 42
+
+fun main() = runBlocking {
+    val deferreds: List<Deferred<Int>> = (1..3).map {
+        async {
+            delay(1000L * it)
+            println("Loading $it")
+            it
+        }
+    }
+    val sum = deferreds.awaitAll().sum()
+    println("$sum")
 }
--- src/tasks/Request2Background.kt
+++ src/tasks/Request2Background.kt
@@ -3,7 +3,6 @@ package tasks
 import contributors.GitHubService
 import contributors.RequestData
 import contributors.User
-import kotlin.concurrent.thread
 
 suspend fun loadContributorsBackground(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     updateResults(loadContributorsBlocking(service, req))
